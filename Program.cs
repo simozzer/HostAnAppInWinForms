0fc68cd5dd8744db7a36e05a693d6d7a742907f0
simozzer <SimonMoscrop@gmail.com>
2024-10-06 23:45:20 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,22 +15,3 @@ public class Program
     }
 }
 
-
-/*
-static class Program
-{
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
-    {
-
-
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
-    }    
-}
-*/
